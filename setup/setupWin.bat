87ec27eeb7de5f6685a44cc5fece5a81504237fc
Stepan Fatov <stepan.fatov.st@vsb.cz>
2024-12-25 23:48:45 +0100

    add script to setup project

---
+++ setup/setupWin.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Set up a virtual environment
+echo Creating a virtual environment...
+python -m venv SmartTerrarium
+
+REM Activate the virtual environment
+call SmartTerrarium\Scripts\activate
+
+REM Install required packages
+echo Installing required packages...
+pip install -r setup\requirements.txt
