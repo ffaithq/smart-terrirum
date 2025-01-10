74b5221cd8a3c0e32623402b62f857a98aa6307a
Stepan Fatov <stepan.fatov.st@vsb.cz>
2025-01-11 00:26:45 +0100

    Add httpHandler, databaseHandler, MQTTHandler

--- Server/main.py
+++ Server/main.py
@@ -1,39 +1,16 @@
-from flask import Flask,request,jsonify,render_template
-import sqlite3
+from http_server import HTTPClientHandler
+from mqtt import MQTTClientHandler
 import threading
 
-app = Flask(__name__, static_folder="static", template_folder="templates")
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/todb", method=['POST'])
-def saveToDb():
-    #TODO create function to save request
-    pass
-
-@app.route('/fromDb',method=['GET'])
-def readToDb():
-    #TODO read frame of db
-    pass
-
-@app.route('/sensors',method=['GET'])
-def readToDb():
-    #TODO return sensors
-    pass
-
-@app.route('/setpoints',method=['GET'])
-def readToDb():
-    #TODO return setpoints
-    pass
-
-
-def run_server():
-    app.run(host="0.0.0.0", port=8000, debug=False)
-
-
 if __name__ == "__main__":
-    server_thread = threading.Thread(target=run_server, daemon=True)
-    server_thread.start()
-    server_thread.join()
+    mqtt = MQTTClientHandler()
+    http = HTTPClientHandler()
+    
+    http_thread = threading.Thread(target=http.run_server, daemon=True)
+    mqtt_thread = threading.Thread(target=mqtt.run_server, daemon=True)
+
+    http_thread.start()
+    mqtt_thread.start()
+
+    http_thread.join()
+    mqtt_thread.join()
--- Server/mqtt.py
+++ Server/mqtt.py
@@ -1,264 +1,188 @@
 import paho.mqtt.client as mqtt
 import time
-import sqlite3
-import os
 import json
+from database import Database
+import logging
+from logger import DatabaseLogHandler
+from DEFINE import *
 
 
-# init database where will be stored ip of esp and that id they got
-def init_db(file_path,script_path):
-    # 0 - means was create successufly
-    # 1 - means database is already exist - NOT ERROR
-    # 2 - error while creating database file - ERROR
-    # 3 - error while connecting - ERROR
-    # 4 - error while creating table - ERROR
-    if os.path.exists(file_path):
-        with open(script_path, 'r') as f:
-            sql_script = f.read()
-
-        conn = sqlite3.connect(file_path)
-        cursor = conn.cursor()
-        # 3. Execute the entire script
-        cursor.executescript(sql_script)
-        # Commit changes to the database
-        conn.commit()
-        return 1
-
-    try:
-        # Connect to SQLite database (or create it if it doesn't exist)
-        conn = sqlite3.connect(file_path)
-    except:
-        return 2
-
-    try:    
-        # Create a cursor object to execute SQL queries
-        cursor = conn.cursor()
-    except:
-        return 3
-
-    try:
-        with open(script_path, 'r') as f:
-            sql_script = f.read()
-
-        # 3. Execute the entire script
-        cursor.executescript(sql_script)
-        # Commit changes to the database
-        conn.commit()
-    except:
-        return 4
-    
-    return 0
+#logger.warning("Warning message")
+#logger.error("Error message")
+# TODO: SETUP JSON FILE
+# TODO: Documentation
+# TODO: Improve logging
 
 
-def get_connection(file_path):
-    if not os.path.exists(file_path):
-        return None
-    conn = sqlite3.connect(file_path)
-    conn.row_factory = sqlite3.Row  # Allows rows to be accessed as dictionaries
-    return conn
+class MQTTClientHandler:
+    def __init__(self):
 
-def on_publish(client, userdata, mid):
-    """
-    Called when a message that was to be sent using the publish() call 
-    has completed transmission to the broker.
-    """
-    print(f"[on_publish] Message ID: {mid}")
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2) #this should be a unique name
+        self.client.on_connect = self.on_connect
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_publish = self.on_publish
+        self.client.message_callback_add('rpi/conn_esp', self.callback_conn_esp)
+        self.client.message_callback_add('rpi/list_ids', self.callback_list_ids)
+        self.client.connect('192.168.0.175',1883)
+        self.logger = self.setup_logger(DATABASE_LOG)
+        self.devices = Database(path=DATABASE_DEVICES,
+                          init_script=INIT_SCRIPT_DEVICES)
+        self.measurement = Database(path=DATABASE_MEASUREMENT,
+                          init_script=INIT_SCRIPT_MEASUREMENT)
+        self.flag_connected = 0
 
-# a callback functions 
-def callback_list_ids(client, userdata, msg):
-    print('List of ids: ', msg.payload.decode('utf-8'))
+    def setup_logger(self,db_file):
 
-def get_ids_list():
-    """
-    Retrieve all rows of `ip` and `id` from the specified table,
-    and return them as a JSON string.
-    """
-    # Connect to the database
-    conn = get_connection('SW/database/devices.db')
-    cursor = conn.cursor()
+        logger = logging.getLogger("DatabaseLogger")
+        logger.setLevel(logging.DEBUG)
 
-    # Execute a query to select IP and ID
-    cursor.execute(f"SELECT ip, id FROM devices")
-    # Fetch all rows
-    rows = cursor.fetchall()
-    # rows will look like: [("192.168.0.10", 1), ("192.168.0.11", 2), ...]
+        db_handler = DatabaseLogHandler(db_file)
+        formatter = logging.Formatter('%(levelname)s - %(message)s')
+        db_handler.setFormatter(formatter)
 
-    # Convert rows to a list of dictionaries
-    results = {}
-    for row in rows:
-        ip, device_id = row
-        results[ip] = device_id
+        logger.addHandler(db_handler)
+        return logger
 
-    # Close the database connection
-    cursor.close()
-    conn.close()
+    def on_connect(self,client, userdata, flags, reason_code, properties):
+        if reason_code == 0:
+            self.logger.info(f"[on connection] Connected successfully!")
+            self.flag_connected = 1
+            self.client_subscriptions()
+        else:
+            self.logger.error(f"[on connection] Connection failed with reason code {reason_code}")
 
-    # Convert to JSON
-    json_data = json.dumps(results, indent=4)
-    return json_data
 
-def publish_ids_list(client, qos=0, retain=False):
-    msg = get_ids_list()
-    pubMsg = client.publish(
-        topic='rpi/list_ids',
-        payload=msg.encode('utf-8'),
-        qos=0,
-    )
-    pubMsg.wait_for_publish()
+    def on_disconnect(self,client, userdata, flags, reason_code, properties):
+        self.flag_connected = 0
+        self.logger.info(f"Disconnected from MQTT server")
 
-def check_ip_in_db(ip_address):
-    # Connect to the SQLite database
-    conn = sqlite3.connect('SW/database/devices.db')
-    cursor = conn.cursor()
-    
-    # Query to check if the IP exists
-    query = "SELECT id FROM devices WHERE ip = ?"
-    cursor.execute(query, (ip_address,))
-    result = cursor.fetchone()
-    
-    # Close the connection
-    conn.close()
-    
-    # Return the ID if the IP exists, otherwise return None
-    if result:
-        return result[0]
-    else:
-        return None
+    def on_publish(self,client, userdata, mid, reason_code, properties):
+        """
+        Called when a message that was to be sent using the publish() call 
+        has completed transmission to the broker.
+        """
 
-def client_subscriptions(client):
-    # TODO: add more subscriptions
-    client.subscribe("rpi/conn_esp")
-    client.subscribe("rpi/list_ids")
+        self.logger.info(f"[on_publish] Message ID: {mid}")
 
-def on_connect(client, userdata, flags, rc):
-   global flag_connected
-   flag_connected = 1
-   client_subscriptions(client)
-   print("Connected to MQTT server")
+    # a callback functions 
+    def callback_list_ids(self,client, userdata, msg):
+        self.logger.info(f'List of ids: {msg.payload.decode("utf-8")}')
 
-def on_disconnect(client, userdata, rc):
-   global flag_connected
-   flag_connected = 0
-   print("Disconnected from MQTT server")
+    def __decode_id(self,rows) -> json:
+        results = {}
+        if rows is not None:
+            for row in rows:
+                ip, device_id = row
+                results[ip] = device_id
 
-def lookup_id(ip):
-    conn = get_connection('SW/database/measurement.db')
-    cursor = conn.cursor()
-    cursor.execute("""
-        SELECT id 
-        FROM devices 
-        WHERE ip = ?
-    """, (ip,))
-    id = cursor.fetchone()[0]
-    if id is None:
-        cursor.close()
-        conn.close()
-        return None
-    
-    cursor.close()
-    conn.close()
-    return id
 
-def callback_esp32(client, userdata, msg):
-    try:
-        conn = get_connection('SW/database/measurement.db')
-    except ValueError as e:
-        print("Caught an error while getting connection to database 'database/measurement.db', error:", e)
+        json_data = json.dumps(results, indent=4)
+        return json_data
 
-    try:
-        cursor = conn.cursor()
-    except ValueError as e:
-        print("Caught an error while getting cursor to database 'database/measurement.db', error:", e)
-        conn.close()
-        return
-    
-    try:
-        # TODO: add more fields to the table
-        payload_str = msg.payload.decode('utf-8')
-        msg_json = json.loads(payload_str)
-        id = msg_json['deviceId']
+    def __asign_id(self,ip):
+        id = self.devices.read_data(SCRIPT_SEARCH_ID,data=ip)
+        status = 0
         if id is None:
-            print("ID is None")
-            return
-        #id = lookup_id(ip)
-        temperature = msg_json['temperature']
-        time_of_measurement = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-        cursor.execute("INSERT INTO measurement (id, temperature, time_of_measurement) VALUES (?, ?, ?)", (id,temperature,time_of_measurement))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        print(f"Inserted data into database for ID {id} with temperature {temperature} and time {time_of_measurement}")
-    except json.JSONDecodeError:
-        print("Failed to decode JSON from payload:", msg.payload)
+            status = 1
+            id = self.devices.read_data(SCRIPT_MAX_ID)[0]
+            if id is None:
+                id = 0
+                id += 1
 
-def callback_conn_esp(c, userdata, msg):
-    try:
-        conn = get_connection('SW/database/devices.db')
-    except ValueError as e:
-        print("Caught an error while getting connection to database 'database/devices.db', error:", e)
+        else:
+            id = id[0]
+        return id,status
 
-    try:
-        cursor = conn.cursor()
-    except ValueError as e:
-        print("Caught an error while getting cursor to database 'database/devices.db', error:", e)
-        conn.close()
-        return
+    def publish_ids_list(self,client, qos=0, retain=False):
+        rows = self.devices.read_data(query=SCRIPT_READ_ID,fetch='all')
 
+        msg = self.__decode_id(rows=rows)
 
-    
-    try:
-        payload_str = msg.payload.decode('utf-8')
-        msg_json = json.loads(payload_str)
-        ip = msg_json['IP']
-        desription = msg_json['Description']
+        pubMsg = client.publish(
+            topic='rpi/list_ids',
+            payload=msg.encode('utf-8'),
+            qos=0,
+        )
+        pubMsg.wait_for_publish()
+
+    def client_subscriptions(self):
+        # TODO: add more subscriptions
+        self.client.subscribe("rpi/conn_esp")
+        self.client.subscribe("rpi/list_ids")
+
+    def callback_esp32(self,client, userdata, msg):    
         try:
-            last_id = check_ip_in_db(ip)
-            if last_id is None:
-                cursor.execute("SELECT MAX(id) FROM devices")
-                # 4. Fetch the result
-                last_id = cursor.fetchone()  # returns a tuple, e.g., (42,) if 42 is the max id
-                if last_id[0] is None:
-                    last_id = 0
-                    id = last_id + 1
-                    time_of_connection = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-                    cursor.execute("INSERT INTO devices (ip, id, descirption, time_of_connection) VALUES (?, ?, ?, ?)", (ip,id,desription,time_of_connection))
-                    conn.commit()
-                    cursor.close()
-                    conn.close()
-                    client.subscribe(f"esp/id_{id}")
-                    print(f"esp/id_{id}")
-                    client.message_callback_add(f"esp/id_{id}", callback_esp32)
-                    print(f"Inserted data into database for IP {ip} with id {id} and time {time_of_connection}")
-            else:
-                print(f"Record with IP {ip} already exists in the database")
-                client.subscribe(f"esp/id_{last_id}")
-                client.message_callback_add(f"esp/id_{last_id}", callback_esp32)
-        except ValueError as e:
-            print("Caught an error while getting last id from database 'database/devices.db', error:", e)
-            conn.close()
-            return
+            payload_str = msg.payload.decode('utf-8')
+            msg_json = json.loads(payload_str)
+            id = msg_json['deviceId']
+            if id is None:
+                self.logger.info("ID is None")
+                return
+            
+            temperature = msg_json['temperature']
+            time_of_measurement = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 
-    except json.JSONDecodeError:
-        print("Failed to decode JSON from payload:", msg.payload)
+            self.measurement.write_data(script=SCRIPT_INSERT_MEASUREMENT,
+                                        data=(id,temperature,time_of_measurement))
+            
+            self.logger.info(f"Inserted data into database for ID {id} with temperature {temperature} and time {time_of_measurement}")
+        except json.JSONDecodeError:
+            self.logger.info(f"Failed to decode JSON from payload:{payload_str}")
+
+    def callback_conn_esp(self,c, userdata, msg):
+        """
+            Callback function. Execute as callback on topic rpi/conn_esp. 
+            While start up ESP32 will always send first request on this topic. 
+            Handshake between RPI and ESP.
+            
+            Arguments:
+                C - client: passed by paho function
+                userdata - userdata which are passed by paho function
+                msg - message which was arrived on topic rpi/conn_esp
+
+            Return:
+                Nothing
+        """
+        try:
+    
+            payload_str = msg.payload.decode('utf-8')
+            msg_json = json.loads(payload_str)
+
+            ip = msg_json['IP']
+            desription = msg_json['Description']
+            time_of_connection = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+            id,status = self.__asign_id(ip=ip)
+
+            if status == 1:
+                self.logger.info(f"esp/id_{id}")
+                self.devices.write_data(SCRIPT_INSERT_DEVICES,(ip,id,desription,time_of_connection))
+                self.logger.info(f"Inserted data into database for IP {ip} with id {id} and time {time_of_connection}")
+            if status == 0:
+                self.logger.info(f"Record with IP {ip} already exists in the database")
 
 
-init_db('SW/database/devices.db','SW/sql/device_init.sql')
-init_db('SW/database/measurement.db','SW/sql/measurement_init.sql')
-client = mqtt.Client("rpi") #this should be a unique name
-flag_connected = 0
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_publish = on_publish
-client.message_callback_add('rpi/conn_esp', callback_conn_esp)
-client.message_callback_add('rpi/list_ids', callback_list_ids)
-client.connect('192.168.0.175',1883)
+            self.client.subscribe(f"esp/id_{id}")
+            self.client.message_callback_add(f"esp/id_{id}", self.callback_esp32)
 
-client.loop_start()
-client_subscriptions(client)
-print("......client setup complete............")
+        except json.JSONDecodeError:
+            self.logger.info(f"Failed to decode JSON from payload: {msg.payload}")
 
-while True:
-    time.sleep(1)
-    publish_ids_list(client)
-    if (flag_connected != 1):
-        print("trying to connect MQTT server..")
+    def run_server(self):
+        self.client.loop_start()
+        self.client_subscriptions()
+        self.logger.info(f"client setup complete")
+
+        while True:
+            time.sleep(1)
+            self.publish_ids_list(self.client)
+            if (self.flag_connected != 1):
+                self.logger.info(f"trying to connect MQTT server..")
+
+
+
+
+
+
+
+
